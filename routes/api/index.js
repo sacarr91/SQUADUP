95556f2225e4faa76ca3f4c2e3f7806369010f89
Stephanie Carr <sacarr91@gmail.com>
2024-08-04 14:43:28 -0400

    routes from mini project

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const courseRoutes = require('./courseRoutes');
+const studentRoutes = require('./studentRoutes');
+
+router.use('/courses', courseRoutes);
+router.use('/students', studentRoutes);
+
+module.exports = router;
+
+// next traffic cop
